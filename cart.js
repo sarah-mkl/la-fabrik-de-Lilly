d1edfa9284ade128ea10a7aff57edd75905d5368
sarah-mkl <sarah.59650@hotmail.fr>
2026-05-16 13:10:42 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,6 +1,9 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
 function addToCart(name, price) {
-  cart.push({ name, price });
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart.push({ name: name, price: price });
+
   localStorage.setItem("cart", JSON.stringify(cart));
+
   alert(name + " ajouté au panier 🛒");
+}
